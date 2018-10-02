e9566f9ebe5ca4724fd12e631aebb667af0f3bee
Eoghan <eoghan.gallagher@gmail.com>
2018-10-02 18:29:45 +0100

    Test Build

---
+++ Assets/Ball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour 
+{
+	private new Rigidbody2D rigidBody2D;
+	[SerializeField] private Vector2 direction;
+
+	[SerializeField] private float moveSpeed = 10f;
+
+	private SpriteRenderer renderer;
+
+	private void Awake()
+	{
+		rigidBody2D = GetComponent<Rigidbody2D>();
+		renderer = GetComponent<SpriteRenderer>();
+	}
+
+	void Start()
+	{
+		Vector2 direction = new Vector2( Random.Range( 0f, 1f ), Random.Range( 0f , 1f ) );
+		rigidBody2D.AddForce( direction );
+		renderer.material.color  = new Color( Random.Range( 0f, 1.0f ), Random.Range( 0f, 1.0f ), Random.Range( 0f, 1.0f ), 1.0f );
+	}
+
+	private void Update()
+	{
+		rigidBody2D.velocity = rigidBody2D.velocity.normalized * moveSpeed;
+	
+	}
+}
